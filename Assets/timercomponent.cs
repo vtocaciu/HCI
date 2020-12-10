a2de70b80dd89eff409beaa7b82bfd2abfd9924c
Valeria <valeria.tocaciu@gmail.com>
2020-12-10 18:05:07 +0200

    First commit

---
+++ Assets/timercomponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class timercomponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayLoadScene(6));
+    }
+
+    public IEnumerator DelayLoadScene(float seconds)
+    {
+        float secondsLeft = seconds;
+        bool loadingStarted = true;
+        do
+        {
+            yield return new WaitForSeconds(1);
+        } while (--secondsLeft > 0);
+
+        SceneManager.LoadScene("New Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
