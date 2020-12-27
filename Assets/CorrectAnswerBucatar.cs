ab141e83e4549aad4171ed019dfe648f6ad3e2f5
Razvan Baboiu <razvanbaboiu@gmail.com>
2020-12-27 14:46:50 +0200

    Add game where you choose the right character for the given chef tools

---
+++ Assets/CorrectAnswerBucatar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bucatarGameCorrectAnswer : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void onCorrectAnswerClicked()
+    {
+        audioSource.Play();
+    }
+}
