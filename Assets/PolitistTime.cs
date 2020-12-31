b0434c9bee74ca6431b0f227bb01fd0c3ef1f895
alessiazeic <alessia.zeic@gmail.com>
2020-12-31 13:50:35 +0200

    added politist-invatare

---
+++ Assets/PolitistTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PolitistTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DelayLoadScene(3));
+    }
+
+    public IEnumerator DelayLoadScene(float seconds)
+    {
+        float secondsLeft = seconds;
+        do
+        {
+            yield return new WaitForSeconds(1);
+        } while (--secondsLeft > 0);
+
+        SceneManager.LoadScene("IntroScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
